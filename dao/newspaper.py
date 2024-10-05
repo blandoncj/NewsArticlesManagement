5af2eaad0a2edff2bd6f744e4dbf6bf6b9a075a9
blandoncj <jacoboblandon94@gmail.com>
2024-10-05 11:59:50 -0500

    feat: add upload articles feature, and weekly report

--- dao/newspaper.py
+++ dao/newspaper.py
@@ -112,12 +112,12 @@ class NewspaperDao:
         """
         try:
             with self.db.connection.cursor() as cursor:
-                print(f"Attempting to save article for newspaper_id: {article.newspaper_id}")  # Agregar esta línea
                 cursor.execute(
                     "INSERT INTO articles (newspaper_id, article_count) VALUES (%s, %s)",
                     (article.newspaper_id, article.articles_count),
                 )
                 self.db.connection.commit()
+                return True
         except MySQLdb.Error as e:
             print(f"Error saving article: {e}")
             return False
